832aff02088bdcd442ce4bbaccca0d6fd0fe7dd1
Sarker Ayon <2640c83ayonrumc@gmail.com>
2026-09-22 11:28:05 +0600

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,13 @@
-import DashStats from "@/components/dash-stats"
-import DashLiveLocation from "@/components/dash-live-location"
+import DashLiveLocation from "@/components/dash-live-location";
 
 export default function Page() {
-    return (
-        <section className="px-6 pt-6 pb-12 min-h-dvh">
-            <div className="gap-3 grid sm:grid-cols-4">
-                <div className="space-y-3 col-span-4">
-                    <DashStats className="flex flex-col sm:grid sm:col-span-4" />
-                    <DashLiveLocation />
-                </div>
-            </div>
-        </section>
-    )
+  return (
+    <section className="px-6 pt-6 pb-12 min-h-dvh">
+      <div className="gap-3 grid sm:grid-cols-4">
+        <div className="space-y-3 col-span-4">
+          <DashLiveLocation />
+        </div>
+      </div>
+    </section>
+  );
 }
